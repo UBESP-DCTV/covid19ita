e64e69a68beda1777323f729beeb31318ff310ec
Corrado Lanera <corrado.lanera@gmail.com>
2020-03-09 23:39:51 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,31 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# covid19ita
+
+<!-- badges: start -->
+
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
+[![CRAN
+status](https://www.r-pkg.org/badges/version/covid19ita)](https://CRAN.R-project.org/package=covid19ita)
+<!-- badges: end -->
+
+The goal of covid19ita is to provide a platform for the monitoring of
+Covid-19 infection diffusion in Italy.
+
+## Installation
+
+You can install the development version of `{covid19ita}` from
+[GitHub](https://github.com/) with:
+
+``` r
+# install.packages("remotes")
+remotes::install_github("UBESP-DCTV/covid19ita")
+```
+
+## Code of Conduct
+
+Please note that the ‘covid19ita’ project is released with a
+[Contributor Code of Conduct](CODE_OF_CONDUCT.md). By contributing to
+this project, you agree to abide by its terms.
---
+++ dev/01_start.R
@@ -0,0 +1,68 @@
+# Building a Prod-Ready, Robust Shiny Application.
+#
+# README: each step of the dev files is optional, and you don't have to
+# fill every dev scripts before getting started.
+# 01_start.R should be filled at start.
+# 02_dev.R should be used to keep track of your development during the project.
+# 03_deploy.R should be used once you need to deploy your app.
+#
+#
+########################################
+#### CURRENT FILE: ON START SCRIPT #####
+########################################
+
+## Fill the DESCRIPTION ----
+## Add meta data about your application
+golem::fill_desc(
+  pkg_name = "covid19ita", # The Name of the package containing the App
+  pkg_title = "Covid-19 - Italy", # The Title of the package containing the App
+  pkg_description = "Monitoring platform for Covid-19 infection diffusion in Italy.", # The Description of the package containing the App
+  author_first_name = "Corrado", # Your First Name
+  author_last_name = "Lanera", # Your Last Name
+  author_email = "corrado.lanera@unipd.it", # Your Email
+  repo_url = NULL # The URL of the GitHub Repo (optional)
+)
+
+## Set {golem} options ----
+golem::set_golem_options()
+
+## Create Common Files ----
+## See ?usethis for more information
+usethis::use_ccby_license( name = "UBEP" )  # You can set another license here
+usethis::use_readme_rmd( open = FALSE )
+usethis::use_code_of_conduct()
+usethis::use_lifecycle_badge( "Experimental" )
+usethis::use_cran_badge()
+usethis::use_news_md( open = FALSE )
+usethis::use_package_doc()
+
+## Use git ----
+usethis::use_git()
+usethis::git_vaccinate()
+## commit before to continue...
+usethis::use_github("UBESP-DCTV")
+usethis::use_tidy_github()
+
+
+
+## Init Testing Infrastructure ----
+## Create a template for tests
+golem::use_recommended_tests()
+
+## Use Recommended Packages ----
+golem::use_recommended_deps()
+
+## Favicon ----
+# If you want to change the favicon (default is golem's one)
+golem::remove_favicon()
+golem::use_favicon() # path = "path/to/ico". Can be an online file.
+
+## Add helper functions ----
+golem::use_utils_ui()
+golem::use_utils_server()
+
+# You're now set! ----
+
+# go to dev/02_dev.R
+rstudioapi::navigateToFile( "dev/02_dev.R" )
+
