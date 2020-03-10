16dec0a75deb9591798322339cd657492193ace9
Corrado Lanera <corrado.lanera@gmail.com>
2020-03-10 09:55:54 +0100

    add dependencies (stringr, purrr, readr)

--- data-raw/covid_ita.R
+++ data-raw/covid_ita.R
@@ -1,3 +1,14 @@
 ## code to prepare `covid_ita` dataset goes here
+requireNamespace("purrr", quietly = TRUE)
+requireNamespace("readr", quietly = TRUE)
+requireNamespace("here", quietly = TRUE)
+library(covid19ita)
+
+data_levels <- list("italia", "regioni", "province")
+
+purrr::walk(data_levels, download_dpc, dir = here::here("data-raw"))
+
+data_paths <- here::here("data-raw", paste0(data_levels, ".csv"))
+covid_ita  <- purrr::map(data_paths, readr::read_csv)
 
 usethis::use_data(covid_ita, overwrite = TRUE)
--- dev/02_dev.R
+++ dev/02_dev.R
@@ -15,7 +15,10 @@
 
 ## Dependencies ----
 ## Add one line by package you want to add as dependency
-usethis::use_package( "thinkr" )
+usethis::use_package("stringr")
+usethis::use_package("purrr", type = "Suggests")
+usethis::use_package("readr", type = "Suggests" )
+usethis::use_tibble()
 
 ## Add modules ----
 ## Create a module infrastructure in R/
