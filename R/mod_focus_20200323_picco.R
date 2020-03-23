69a0ec7cc209414218b53833ecbb673afcc09921
Corrado <corrado.lanera@gmail.com>
2020-03-23 18:51:11 +0100

    template

--- R/mod_focus_20200323_picco.R
+++ R/mod_focus_20200323_picco.R
@@ -10,16 +10,40 @@
 mod_focus_20200323_picco_ui <- function(id){
   ns <- NS(id)
   tagList(
+    sliderInput(ns("foo"), "Scegli il tuo parametro p",
+      min = 10,
+      max = 35,
+      value = 5,
+      step  = 1
+    ),
 
+    plotlyOutput(ns("picco"))
   )
 }
 
 #' focus_20200323_picco Server Function
 #'
 #' @noRd
-mod_focus_20200323_picco_server <- function(input, output, session){
-  ns <- session$ns
+mod_focus_20200323_picco_server <- function(id) {
 
+
+  callModule(id = id, function(input, output, session) {
+    ns <- session$ns
+
+    gg_picco <- reactive({
+      req(input$foo)
+
+      mtcars %>%
+        dplyr::filter(.data$mpg > input$foo) %>%
+        ggplot(aes(x = mpg, y = cyl)) +
+        geom_point()
+    })
+
+    output$picco <- renderPlotly({
+      ggplotly(gg_picco())
+    })
+
+  })
 }
 
 ## To be copied in the UI
