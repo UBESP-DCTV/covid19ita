42a65f168bdd2efbc255fd3e05bd2fc170014cc4
Corrado <corrado.lanera@gmail.com>
2020-03-24 12:29:19 +0100

    parametric region (to fix)

--- R/mod_focus_20200323_picco.R
+++ R/mod_focus_20200323_picco.R
@@ -12,7 +12,7 @@ mod_focus_20200323_picco_ui <- function(id){
 
   obs_t <- dpc_covid19_ita_andamento_nazionale[["data"]]
   obs_y <- dpc_covid19_ita_andamento_nazionale[["totale_casi"]]
-  pred_val <- growthcurver::SummarizeGrowth(
+  pred_val_origin <- growthcurver::SummarizeGrowth(
     data_t = seq_along(obs_t),
     data_n = obs_y
   )$vals
@@ -38,7 +38,7 @@ mod_focus_20200323_picco_ui <- function(id){
       step  = round(pred_val$r_se / 10, 4)
     ),
 
-    plotOutput(ns("picco")),
+    plotlyOutput(ns("picco")),
 
     shiny::selectInput(ns("whichRegion"),  "Selezionare le regioni da visualizzare",
       choices  = regions(),
@@ -48,7 +48,7 @@ mod_focus_20200323_picco_ui <- function(id){
       width = "100%"
     ),
 
-    plotOutput(ns("picco_reg")),
+    plotlyOutput(ns("picco_reg")),
 
   )
 }
@@ -60,6 +60,10 @@ mod_focus_20200323_picco_server <- function(id) {
 
   obs_t  <- dpc_covid19_ita_andamento_nazionale[["data"]]
   obs_y <- dpc_covid19_ita_andamento_nazionale[["totale_casi"]]
+  pred_val <- growthcurver::SummarizeGrowth(
+    data_t = seq_along(obs_t),
+    data_n = obs_y
+  )$vals
 
   obs_db <- tibble::tibble(
     t = as.Date(obs_t),
@@ -69,27 +73,34 @@ mod_focus_20200323_picco_server <- function(id) {
   pred_t <- c(obs_t, obs_t[[length(obs_t)]] + lubridate::days(1:28))
 
 
+
   obs_reg <- dpc_covid19_ita_regioni %>%
-    dplyr::select(
-      .data$data, .data$denominazione_regione, .data$totale_casi
+    dplyr::transmute(
+      t = as.Date(.data$data),
+      regione = .data$denominazione_regione,
+      totale_casi = .data$totale_casi
     ) %>%
+    dplyr::group_by(.data$regione) %>%
+    dplyr::arrange(.data$t) %>%
+    dplyr::mutate(
+      y = (.data$totale_casi - dplyr::lag(.data$totale_casi, default = 0))
+    ) %>%
+    dplyr::ungroup()
+
+
+  obs_reg_plate <- obs_reg %>%
+    dplyr::select(-.data$y) %>%
     tidyr::pivot_wider(
-      names_from = .data$denominazione_regione,
+      names_from = .data$regione,
       values_from = .data$totale_casi
     ) %>%
-    dplyr::mutate(time = seq_along(.data$data)) %>%
-    dplyr::select(-.data$data)
+    dplyr::mutate(time = seq_along(.data$t)) %>%
+    dplyr::select(-.data$t)
 
-  pred_val_reg <- growthcurver::SummarizeGrowthByPlate(obs_reg) %>%
+  pred_db_reg <- obs_reg_plate %>%
+    growthcurver::SummarizeGrowthByPlate() %>%
     dplyr::rename(regione = .data$sample) %>%
-    dplyr::mutate(
-      y = purrr::pmap(
-        list(.data$k, .data$n0, .data$r),
-        growthcurver::NAtT,
-        t = seq_along(pred_t)
-      )
-    ) %>%
-    tibble::as_tibble()
+    dplyr::select(.data$regione, .data$k, .data$n0, .data$r)
 
 
   callModule(id = id, function(input, output, session) {
@@ -120,9 +131,40 @@ mod_focus_20200323_picco_server <- function(id) {
 
 
 
-    output$picco <- renderPlot({
 
-      tibble::tibble(t = as.Date(pred_t), y = pred_n()) %>%
+    pred_val_reg <- reactive({
+      k_ita  <- req(input$k)
+      n0_ita <- req(input$n0)
+      r_ita  <- req(input$r)
+
+      pred_db_reg %>%
+       dplyr::mutate(
+         k  = (.data$k  * k_ita) / pred_val_origin[["k"]],
+         n0 = (.data$n0 * k_ita) / pred_val_origin[["n0"]],
+         r  = (.data$r  * k_ita) / pred_val_origin[["r"]],
+
+         natt = purrr::pmap(
+            list(.data$k, .data$n0, .data$r),
+            function(k, n0, r) {
+              tibble::tibble(
+                t = as.Date(pred_t),
+                y = growthcurver::NAtT(k, n0, r, t = seq_along(pred_t))
+              )
+            }
+          )
+        ) %>%
+        tidyr::unnest(cols = .data$natt) %>%
+        dplyr::group_by(.data$regione) %>%
+        dplyr::arrange(.data$t) %>%
+        dplyr::mutate(y = .data$y - dplyr::lag(.data$y, default = 0)) %>%
+        dplyr::ungroup()
+    })
+
+
+
+    output$picco <- renderPlotly({
+
+      gg_ita <- tibble::tibble(t = as.Date(pred_t), y = pred_n()) %>%
         ggplot(aes(x = t, y = y)) +
         geom_point() +
         geom_point(data = obs_db, colour = "red") +
@@ -132,22 +174,32 @@ mod_focus_20200323_picco_server <- function(id) {
         theme(
           axis.text.x = element_text(angle = 60, hjust = 1, vjust = 0.5)
         )
+
+      ggplotly(gg_ita)
 
     })
 
 
-    output$picco_reg <- renderPlot({
+    output$picco_reg <- renderPlotly({
 
-      tibble::tibble(t = as.Date(pred_t), y = pred_n()) %>%
-        ggplot(aes(x = t, y = y)) +
-        geom_point() +
-        geom_point(data = obs_db, colour = "red") +
+      reg <- req(input$whichRegion)
+
+      gg_reg <- pred_val_reg() %>%
+        dplyr::filter(.data$regione %in% reg) %>%
+        ggplot(aes(x = .data$t, y = .data$y, colour = .data$regione)) +
+        geom_point(colour = "black") +
+        geom_point(
+          data = dplyr::filter(obs_reg, .data$regione %in% reg)
+        ) +
+        facet_wrap(~.data$regione, scales = "free") +
         ylab("Numero di nuovi casi") +
         xlab("") +
         scale_x_date(date_breaks = "1 day", date_labels = "%b %d") +
         theme(
           axis.text.x = element_text(angle = 60, hjust = 1, vjust = 0.5)
         )
+
+      ggplotly(gg_reg)
 
     })
 
