cd2efd1026f28f02051eb8a11a1278f5944af643
Francesco <fpirotti@gmail.com>
2020-04-04 18:27:29 +0200

    Updated covid-ita.R file for using recent geometry data

--- data-raw/covid_ita.R
+++ data-raw/covid_ita.R
@@ -58,7 +58,10 @@ if (!all(are_ok)) {
     "Valle d'Aosta"     	 ,               126883
   )
 
-  province_population2019 <- readRDS("data-raw/province_population2019.Rda")
+  province_population2019 <- readRDS("data-raw/province_population2019.rds")
+
+  province_geometry2019 <- readRDS("data-raw/province_geometry2019.rds")
+
   dictionary <- c(
     ricoverati_con_sintomi = "hospitalized_with_symptoms",
     terapia_intensiva = "intensive_care",
@@ -101,6 +104,7 @@ if (!all(are_ok)) {
     last_data_update,
     region_population,
     province_population2019,
+    province_geometry2019,
     dictionary,
     internal = TRUE,
     overwrite = TRUE
