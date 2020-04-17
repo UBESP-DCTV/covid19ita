8006e381c814e6cdd5ea5ceaddbaf1d0296e669a
Corrado <corrado.lanera@gmail.com>
2020-04-17 15:08:35 +0200

    Revert "Remove tests to trigger covr to run on CI"
    This reverts commit 9d22a3d29dec160d26ecffb9c2bc50ce5e2574f4.

--- tests/testthat/test-fct_cumulate_for_days.R
+++ tests/testthat/test-fct_cumulate_for_days.R
@@ -1,16 +1,16 @@
-# test_that("accumulate_for_days works", {
-#   expect_equal(
-#     accumulate_for_days(c(1, 2, 3), 2),
-#     c(1, 3, 5, 3)
-#   )
-#
-#   expect_equal(
-#     accumulate_for_days(c(1, 2, 3), 3),
-#     c(1, 3, 6, 5, 3)
-#   )
-#
-#   expect_equal(
-#     accumulate_for_days(c(NA, 2, 3), 3),
-#     c(0, 2, 5, 5, 3)
-#   )
-# })
+test_that("accumulate_for_days works", {
+  expect_equal(
+    accumulate_for_days(c(1, 2, 3), 2),
+    c(1, 3, 5, 3)
+  )
+
+  expect_equal(
+    accumulate_for_days(c(1, 2, 3), 3),
+    c(1, 3, 6, 5, 3)
+  )
+
+  expect_equal(
+    accumulate_for_days(c(NA, 2, 3), 3),
+    c(0, 2, 5, 5, 3)
+  )
+})
