6d7df6251af49cf14cf32599625905d72985816d
CorradoLanera <corrado.lanera@gmail.com>
2020-10-24 00:50:57 +0200

    Data update

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,5 @@
+* data update
+
 # covid19ita 0.106.0
 
 * data update
--- data-raw/data-UPDATE.R
+++ data-raw/data-UPDATE.R
@@ -9,4 +9,21 @@ source(here::here("data-raw", "data-EXPORTED.R"), local = TRUE)
 rm(list = ls(all.names = TRUE))
 
 devtools::check_man()
-devtools::check()
+res_check <- devtools::check()
+
+
+if (length(res_check$errors) == 0L & length(res_check$warnings) == 0L) {
+
+  nws <- here::here("NEWS.md")
+  readr::write_lines(
+    c(list(
+      "* data update",
+      ""
+    ), readr::read_lines(file = nws)),
+    file = nws
+  )
+
+  git2r::commit(message = "Data update", all = TRUE)
+
+  usethis::use_version("minor")
+}
