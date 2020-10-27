a17004720ad81230a43390a99085e4828b91ad21
Daniele Bottigliengo <daniele.bottigliengo@libero.it>
2020-10-27 15:13:39 +0100

    Add last plots

--- R/mod_icuve_ts.R
+++ R/mod_icuve_ts.R
@@ -30,15 +30,25 @@ mod_icuve_ts_ui <- function(id){
         title = "Figure 2. Andamento stimato (linea rossa in grassetto,
         l'area rossa indica gli intervalli di confidenza al 95%) del
         numero di posti letto totali in terapia intensiva occupati
-        da pazienti Covid. Andamento osservato (punti blu) fino alla
-        data odierna."
+        da pazienti Covid aggiustato per proporzione di tamponi positivi.
+        Andamento osservato (punti blu) fino alla data odierna."
       )
     ),
     fluidRow(
       box(
         width = 12,
         plotly::plotlyOutput(ns("fig3")),
-        title = "Figure 3. Andamento stimato (linea rossa in grassetto,
+        title = "Figure 3. Relazione stimata (linea rossa in grassetto,
+        l'area rossa indica gli intervalli di confidenza al 95%) tra
+        numero di posti letto totali in terapia intensiva occupati
+        da pazienti Covid e proporzione di tamponi positivi."
+      )
+    ),
+    fluidRow(
+      box(
+        width = 12,
+        plotly::plotlyOutput(ns("fig4")),
+        title = "Figure 4. Andamento stimato (linea rossa in grassetto,
         l'area rossa indica gli intervalli di confidenza al 95%) della
         differenza tra il numero di posti letto in terapia intensiva
         in data odierna e 3 giorni precedenti. Andamento osservato
@@ -57,8 +67,31 @@ mod_icuve_ts_ui <- function(id){
 mod_icuve_ts_server <- function(id) {
 
   icuve_ts <- covid19.icuve::fetch_gsheet()
+  covid19dd <- covid19ita::dpc_covid19_ita_regioni
 
   # 1) Prepare the data ------------------------------------------------
+  # Time series Veneto -------------------------------------------------
+  veneto_ts <- covid19dd %>%
+    # Select the Veneto data
+    dplyr::filter(.data$denominazione_regione == "Veneto") %>%
+    # Rename the column date the make it consistent with the other db
+    dplyr::rename(date = data) %>%
+    # Take only the date, the positive and the swabs
+    dplyr::select(.data$date, .data$totale_casi, .data$tamponi) %>%
+    # dplyr::select(date, prop_pos) %>%
+    dplyr::mutate(
+      date = lubridate::as_date(.data$date),
+      lag_casi = dplyr::lag(.data$totale_casi, n = 1L, default = NA),
+      lag_swab = dplyr::lag(.data$tamponi, n = 1L, default = NA),
+      casi = .data$totale_casi - .data$lag_casi,
+      swab = .data$tamponi - .data$lag_swab,
+      prop_pos = .data$casi/.data$swab
+    ) %>%
+    # Take the 1st of September as starting date for the models
+    dplyr::filter(.data$date >= lubridate::ymd("2020-09-01")) %>%
+    dplyr::select(date, prop_pos)
+
+  # Time series data Veneto ICU ----------------------------------------
   df <- icuve_ts %>%
     dplyr::mutate(
       # Proportion of COVID beds
@@ -70,7 +103,9 @@ mod_icuve_ts_server <- function(id) {
       covid_variation = .data$covid_occupied - .data$covid_occ_lag
     ) %>%
     # Take the 1st of September as starting date for the models
-    dplyr::filter(.data$date >= lubridate::ymd("2020-09-01"))
+    dplyr::filter(.data$date >= lubridate::ymd("2020-09-01")) %>%
+    # Join the datasets
+    dplyr::left_join(veneto_ts, by = "date")
 
   # 2) Prepare days ahead ----------------------------------------------
   days_ahead <- 20L
@@ -83,7 +118,18 @@ mod_icuve_ts_server <- function(id) {
       df %>%
         dplyr::slice(seq_along(seq_ahead)) %>%
         dplyr::mutate(date = seq_ahead) %>%
-        dplyr::mutate_at(dplyr::vars(-.data$date), ~ NA_integer_)
+        dplyr::mutate_at(
+          dplyr::vars(-.data$date),
+          ~ NA_integer_
+        )
+    ) %>%
+    # If the prop of positive swabs is missing take the last available
+    dplyr::mutate(
+      prop_pos = dplyr::if_else(
+        is.na(.data$prop_pos),
+        dplyr::last(na.omit(.data$prop_pos)),
+        .data$prop_pos
+      )
     )
 
   # 3) Proportion of COVID beds ----------------------------------------
@@ -134,9 +180,10 @@ mod_icuve_ts_server <- function(id) {
     xlab("") +
     ylab("Proporzione")
 
-  # 4) COVID beds occupied ---------------------------------------------
+  # 4) COVID beds occupied adjusted by proportion of positive ----------
   fit_beds <- mgcv::gam(
-    covid_occupied ~ s(as.numeric(date), bs = "cr"),
+    covid_occupied ~ s(as.numeric(date)) +
+      s(prop_pos),
     data = df,
     family = stats::poisson(link = "log")
   )
@@ -168,8 +215,9 @@ mod_icuve_ts_server <- function(id) {
       name = "",
       values = c("Atteso" = "firebrick2", "Osservato" = "dodgerblue1")
     ) +
+    scale_y_continuous(limits = c(0, 250)) +
     scale_x_date(
-      limits = c(min(df$date), max(df$date) + 10),
+      limits = c(min(df$date), max(df$date) + 7),
       date_breaks = "1 week", date_labels = "%d %b"
     ) +
     theme(
@@ -181,7 +229,50 @@ mod_icuve_ts_server <- function(id) {
     xlab("") +
     ylab("Numero posti letto")
 
-  # 5) Delta days ------------------------------------------------------
+  # 5) COVID beds and by proportion of positive ------------------------
+  fit_swab <- mgcv::gam(
+    covid_occupied ~ s(prop_pos),
+    data = df,
+    family = stats::poisson(link = "log")
+  )
+
+  pred_db_swab <- df %>%
+    dplyr::mutate(
+      swab_pred = stats::predict(fit_swab, .),
+      swab_se = stats::predict(fit_swab, ., se.fit = TRUE)[["se.fit"]]
+    )
+
+  ggswab <- ggplot(
+    data = pred_db_swab, mapping = aes(x = .data$prop_pos)
+  ) +
+    geom_line(
+      mapping = aes(y = exp(.data$swab_pred), color = "Atteso"),
+      size = 1.2
+    ) +
+    geom_ribbon(
+      mapping = aes(
+        ymin = exp(.data$swab_pred - 1.96 * .data$swab_se),
+        ymax = exp(.data$swab_pred + 1.96 * .data$swab_se)
+      ), alpha = 0.33, fill = "firebrick2", color = NA
+    ) +
+    geom_point(
+      mapping = aes(y = .data$covid_occupied, color = "Osservato"),
+      size = 1.8
+    ) +
+    scale_x_continuous(
+      limits = c(
+        stats::quantile(df$prop_pos, 0.1, na.rm = TRUE),
+        stats::quantile(df$prop_pos, 0.9, na.rm = TRUE)
+      )
+    ) +
+    scale_color_manual(
+      name = "",
+      values = c("Atteso" = "firebrick2", "Osservato" = "dodgerblue1")
+    ) +
+    xlab("Proporzione tamponi positivi") +
+    ylab("Numero posti letto")
+
+  # 6) Delta days ------------------------------------------------------
   fit_delta_days <- stats::loess(
     covid_variation ~ as.numeric(date),
     data = df,
@@ -243,6 +334,10 @@ mod_icuve_ts_server <- function(id) {
       })
 
       output$fig3 <- plotly::renderPlotly({
+        plotly::ggplotly(ggswab)
+      })
+
+      output$fig4 <- plotly::renderPlotly({
         plotly::ggplotly(ggdelta_days)
       })
 
