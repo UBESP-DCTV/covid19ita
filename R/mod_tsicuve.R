2a8c0bbf82c951d844422d8448336c11a5082a85
Daniele Bottigliengo <daniele.bottigliengo@libero.it>
2020-11-20 01:21:40 +0100

    The app runs!

--- R/mod_tsicuve.R
+++ R/mod_tsicuve.R
@@ -39,7 +39,7 @@ mod_tsicuve_ui <- function(id){
       )
     ),
     fluidRow(
-      h2(HTML("2) Damped")),
+      h2(HTML("2) Exponential smoothing state space model")),
       sliderInput(
         width = "45%", ns("lastDate"),
         label = "Selezionare l'ultima data da considerare per la stima del modello",
@@ -56,12 +56,44 @@ mod_tsicuve_ui <- function(id){
           title = "Figura 2A. Andamento stimato (linea rossa in grassetto,
         l'area rossa indica gli intervalli di confidenza al 95%) del
         numero di posti occupati in terapia intensiva.
-        Andamento osservato (punti blu) fino alla data odierna."
+        Andamento osservato (punti blu) fino alla data odierna.",
+          footer = "NOTE: il modello è stato stimato ipotizzando un damped trend."
       ),
       box(plotlyOutput(ns("fig2b")),
           width = 12,
           title = "Figura 2B. Andamento dell'errore quadratico del
-        modello fino alla data odierna."
+        modello fino alla data odierna.",
+          footer = "NOTE: il modello è stato stimato ipotizzando un damped trend."
+      )
+    ),
+    fluidRow(
+      h2(HTML("3) ARIMA")),
+      sliderInput(
+        width = "45%", ns("lastDate"),
+        label = "Selezionare l'ultima data da considerare per la stima del modello",
+        value = lubridate::as_date(max(dpc_covid19_ita_regioni$data)),
+        min = lubridate::as_date(min(dpc_covid19_ita_regioni$data)) +
+          lubridate::days(10),
+        max = lubridate::as_date(max(dpc_covid19_ita_regioni$data)) -
+          lubridate::days(8),
+        step = lubridate::days(1),
+        animate = animationOptions(interval = 400)
+      ),
+      box(plotlyOutput(ns("fig3a")),
+          width = 12,
+          title = "Figura 3A. Andamento stimato (linea rossa in grassetto,
+        l'area rossa indica gli intervalli di confidenza al 95%) del
+        numero di posti occupati in terapia intensiva.
+        Andamento osservato (punti blu) fino alla data odierna.",
+          footer = "NOTE: il modello è stato stimato con un metodo automatico basato
+          sull'AIC corretto."
+      ),
+      box(plotlyOutput(ns("fig3b")),
+          width = 12,
+          title = "Figura 3B. Andamento dell'errore quadratico del
+        modello fino alla data odierna.",
+          footer = "NOTE: il modello è stato stimato con un metodo automatico basato
+          sull'AIC corretto."
       )
     )
   )
@@ -136,6 +168,26 @@ mod_tsicuve_server <- function(id) {
       )
     )
 
+  # 2C) ARIMA ----------------------------------------------------------
+  df_error <- purrr::map_dfr(
+    .x = d_seq, ~ arima_error(veneto, n_ahead, .x, tstart)
+  )
+
+  error_arima <- ggplot(
+    data = df_error,
+    mapping = aes(x = .data$data, y = .data$error)
+  ) +
+    geom_point(size = 1.1) +
+    geom_smooth(se = FALSE) +
+    ylab("Squared error") +
+    xlab("") +
+    scale_x_date(date_breaks = "2 weeks", date_labels = "%d %b") +
+    theme(
+      axis.text.x = element_text(
+        angle = 60, hjust = 1, vjust = 0.5
+      )
+    )
+
 
 
   callModule(id = id, function(input, output, session) {
@@ -163,7 +215,7 @@ mod_tsicuve_server <- function(id) {
     })
 
     output$fig2a <- renderPlotly({
-      gg_holter <- damped_plot(
+      gg_damped <- damped_plot(
         data = dpc_covid19_ita_regioni %>%
           # Get the Veneto ICU data
           dplyr::filter(.data$denominazione_regione == "Veneto") %>%
@@ -176,11 +228,32 @@ mod_tsicuve_server <- function(id) {
         tstop = input$lastDate
       )
 
-      ggplotly(gg_holter, originalData = FALSE)
+      ggplotly(gg_damped, originalData = FALSE)
     })
 
     output$fig2b <- plotly::renderPlotly({
       plotly::ggplotly(error_damped)
+    })
+
+    output$fig3a <- renderPlotly({
+      gg_arima <- arima_plot(
+        data = dpc_covid19_ita_regioni %>%
+          # Get the Veneto ICU data
+          dplyr::filter(.data$denominazione_regione == "Veneto") %>%
+          # Select relevant variables
+          dplyr::select(.data$data, .data$terapia_intensiva) %>%
+          # Dates in lubridate format
+          dplyr::mutate(data = lubridate::as_date(.data$data)),
+        n_ahead = n_ahead,
+        tstart = tstart,
+        tstop = input$lastDate
+      )
+
+      ggplotly(gg_arima, originalData = FALSE)
+    })
+
+    output$fig3b <- plotly::renderPlotly({
+      plotly::ggplotly(error_arima)
     })
 
   })
--- R/utils_tsicuve.R
+++ R/utils_tsicuve.R
@@ -159,10 +159,7 @@ damped_plot <- function(data, n_ahead, tstart, tstop) {
   # Fit the model
   hw_object <- forecast::ets(my_ts, damped = TRUE)
 
-  pred <- stats::predict(
-    hw_object,  h = n_ahead,  prediction.interval = TRUE,
-    level = 0.95
-  )
+  pred <- forecast::forecast(hw_object,  h = n_ahead)
 
   # Prepare the dataframes for the TS plot
   na_ahead <- length(data$data) -
@@ -182,11 +179,11 @@ damped_plot <- function(data, n_ahead, tstart, tstop) {
     dplyr::mutate(
       lower = c(
         rep(NA_real_, length(as.double(hw_object$fitted))),
-        as.double(pred$lower[, 1])
+        as.double(pred$lower[, 2])
       ),
       upper = c(
         rep(NA_real_, length(as.double(hw_object$fitted))),
-        as.double(pred$upper[, 1])
+        as.double(pred$upper[, 2])
       )
     ) %>%
     dplyr::mutate(
@@ -261,10 +258,7 @@ damped_error <- function(data, n_ahead, d, tstart) {
   # Fit the model
   hw_object <- forecast::ets(my_ts, damped = TRUE)
 
-  pred <- stats::predict(
-    hw_object,  h = n_ahead,  prediction.interval = TRUE,
-    level = 0.95
-  )
+  pred <- forecast::forecast(hw_object,  h = n_ahead)
 
   # Construct data for error
   expected <- round(c(hw_object$fitted, pred$mean))
@@ -283,3 +277,144 @@ damped_error <- function(data, n_ahead, d, tstart) {
   )
 
 }
+
+arima_plot <- function(data, n_ahead, tstart, tstop) {
+
+  assertive::assert_is_data.frame(data)
+  assertive::assert_is_integer(n_ahead)
+  assertive::assert_is_date(tstart)
+  assertive::assert_is_date(tstop)
+
+  # Select the time series for model fitting
+  ts_fit <- data %>%
+    dplyr::filter(.data$data >= tstart & .data$data <= tstop)
+
+  # Retrieve the time-series
+  my_ts <- stats::ts(
+    ts_fit[["terapia_intensiva"]],
+    start = c(2020, as.numeric(format(min(ts_fit[["data"]]), "%j"))),
+    end = c(2020, as.numeric(format(max(ts_fit[["data"]]), "%j")))
+  )
+
+  # Fit the model
+  hw_object <- forecast::auto.arima(my_ts)
+
+  pred <- forecast::forecast(hw_object,  h = n_ahead)
+
+  # Prepare the dataframes for the TS plot
+  na_ahead <- length(data$data) -
+    lubridate::interval(tstart, tstop)/lubridate::ddays(1) -
+    n_ahead
+
+  obs_df <- data %>%
+    dplyr::rename(est = .data$terapia_intensiva)
+
+  fitted_df <- tibble::tibble(
+    data = seq(from = tstart, to = tstop + n_ahead, by = 1),
+    est = round(c(
+      as.double(hw_object$fitted),
+      as.double(pred$mean)
+    ))
+  ) %>%
+    dplyr::mutate(
+      lower = c(
+        rep(NA_real_, length(as.double(hw_object$fitted))),
+        as.double(pred$lower[, 2])
+      ),
+      upper = c(
+        rep(NA_real_, length(as.double(hw_object$fitted))),
+        as.double(pred$upper[, 2])
+      )
+    ) %>%
+    dplyr::mutate(
+      lower = dplyr::if_else(
+        is.na(.data$lower), .data$est, .data$lower
+      ),
+      upper = dplyr::if_else(
+        is.na(.data$upper), .data$est, .data$upper
+      ),
+    ) %>%
+    # If upper or lower are less than 0 put 0
+    dplyr::mutate(
+      lower = dplyr::if_else(.data$lower < 0, 0, .data$lower),
+      upper = dplyr::if_else(.data$upper < 0, 0, .data$upper)
+    )
+
+
+  # TS plot
+  ggplot(
+    data = fitted_df,
+    mapping = aes(x = .data$data)
+  ) +
+    geom_line(
+      mapping = aes(y = .data$est, colour = "Atteso"),
+      size = 1.5
+    ) +
+    geom_line(
+      data = obs_df,
+      mapping = aes(y = .data$est, color = "Osservato"),
+      size = 0.8
+    ) +
+    geom_ribbon(
+      mapping = aes(ymin = .data$lower, ymax = .data$upper),
+      alpha = 0.2, fill = "firebrick2", colour = NA
+    ) +
+    scale_color_manual(
+      name = "",
+      values = c("Atteso" = "firebrick2", "Osservato" = "dodgerblue1")
+    ) +
+    ylab("Numero posti letto TI") +
+    xlab("") +
+    scale_x_date(date_breaks = "2 weeks", date_labels = "%d %b") +
+    theme(
+      axis.text.x = element_text(
+        angle = 60, hjust = 1, vjust = 0.5
+      )
+    )
+
+}
+
+arima_error <- function(data, n_ahead, d, tstart) {
+
+  assertive::assert_is_data.frame(data)
+  assertive::assert_is_integer(n_ahead)
+  assertive::assert_is_integer(d)
+  assertive::assert_is_date(tstart)
+
+  # Maximum date for TS model fitting
+  tstop <- tstart + d
+
+  # Select the time series for model fitting
+  ts_fit <- data %>%
+    dplyr::filter(.data$data >= tstart & .data$data <= tstop)
+
+  # Retrieve the time-series
+  my_ts <- stats::ts(
+    ts_fit[["terapia_intensiva"]],
+    start = c(2020, as.numeric(format(min(ts_fit[["data"]]), "%j"))),
+    end = c(2020, as.numeric(format(max(ts_fit[["data"]]), "%j")))
+  )
+
+  # Fit the model
+  hw_object <- forecast::auto.arima(my_ts)
+
+  pred <- forecast::forecast(hw_object,  h = n_ahead)
+
+  # Construct data for error
+  expected <- round(c(hw_object$fitted, pred$mean))
+
+  tt <- data %>%
+    dplyr::filter(.data$data <= max(ts_fit$data) + n_ahead)
+  obs <- tt$terapia_intensiva
+
+  # Squared error for count data
+  sq_err <- tscount::scoring(expected, obs)[[7]]
+
+  # Final result into a tibble
+  tibble::tibble(
+    data = max(ts_fit$data) + n_ahead,
+    error = sq_err
+  )
+
+}
+
