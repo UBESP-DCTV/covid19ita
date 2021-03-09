7efa5fd3231bfb2ae7c8dac4c526f8ca8fe138db
CorradoLanera <corrado.lanera@gmail.com>
2021-03-09 11:31:42 +0100

    Much faster load (conditional load on tabs)

--- R/app_server.R
+++ R/app_server.R
@@ -6,13 +6,6 @@
 #' @noRd
 app_server <- function(input, output, session) {
 
-
-
-
-
-
-
-
   ############
   ## LOGIN #############################################################
   ############
@@ -84,6 +77,66 @@ app_server <- function(input, output, session) {
   observe({
     req(USER$login)
 
+    observeEvent(input$sidebar,{
+      switch(input$sidebar,
+
+        # Andamento epidemia ------------------------------------------
+        national = {
+          mod_ts_ita_server("ts_nat_cum", "cum")
+          mod_ts_ita_server("ts_nat_inc", "inc")
+        },
+        regional = {
+          mod_ts_reg_server("ts_reg_cum_mes", "cum", color_var = "measure")
+          mod_ts_reg_server("ts_reg_inc_mes", "inc", color_var = "measure")
+          mod_ts_reg_server("ts_reg_cum_reg", "cum", color_var = "region")
+          mod_ts_reg_server("ts_reg_inc_reg", "inc", color_var = "region")
+        },
+        provincial = {
+          mod_ts_prv_server("ts_prv_cum", "cum")
+          mod_ts_prv_server("ts_prv_inc", "inc")
+        },
+
+        # Terapie intensive -------------------------------------------
+        "regional-partial-ts-icuve" = mod_reg_tsicuve_server("regional_partial_tsicuve"),
+        "regional-partial-ts-nocritica" = mod_reg_nocritica_server("regional_partial_nocritica"),
+        "azero-sitrep" = mod_azero_server("azero_sitrep"),
+        "regional-icuve-sitrep" = mod_icuve_sitrep_server("icuve_sitrep"),
+        "regional-icuve-ts" = mod_icuve_ts_server("icuve_ts"),
+        "regional-icuve-static" = mod_icuve_static_server("icuve_static"),
+
+        # Analisi tematiche -------------------------------------------
+        "partial-ts-icuve" =  mod_tsicuve_server("partial_ts_icuve"),
+
+        # Indici principali -------------------------------------------
+        "impact" = mod_ind_ita_server("20200315"),
+
+        # Mappe -------------------------------------------------------
+        "geo_spatialTot" = mod_maps_server("geo_1"),
+
+        # Archivio analisi --------------------------------------------
+        "20200415TampHosp" = mod_0415_tamponi_server("tamp_hosp"),
+        "20200406MortVeneto" = mod_0406_mort_veneto_server("mort_veneto"),
+        "20200405Mort" = mod_0404_magnani_server("mortality"),
+        "20200331Comp" = mod_0331_server("ven_pie"),
+        "20200328Tot_hosp" = mod_0328_hosp_server("tot"),
+        "20200325Hosp" = mod_0325_hosp_server("hosp"),
+        "20200323Picco" = mod_0323_picco_server("picco"),
+        "20200321Alessandria" = mod_0320_novara_server("da_alessandria",
+                                                       loc = "Alessandria",
+                                                       pop = 428826
+        ),
+        "20200321Vercelli" = mod_0320_novara_server("da_vercelli",
+                                                    loc = "Vercelli",
+                                                    pop = 174904
+        ),
+        "20200320Novara" = mod_0320_novara_server("da_novara"),
+        "20200319Veneto" = mod_0318_intensive_server("21"),
+        "20200318Piemonte" = mod_0318_piemonte_server("20200318_piemonte"),
+        "20200318Fvg" = mod_0318_friuli_server("20200318_fvg"),
+        "20200314Veneto" = mod_0314_server("dapb")
+      )
+    }, ignoreNULL = TRUE, ignoreInit = TRUE)
+
     ## Header info
     mod_img_header_server("logo_testa", "Covid19.png")
     mod_img_header_server("logo_coda_torino", "Torino.png")
@@ -91,66 +144,23 @@ app_server <- function(input, output, session) {
     mod_img_header_server("logo_coda", "Covid19.png")
     mod_info_sidebar_server("summary_today")
 
-    ## Impact
-    mod_ind_ita_server("20200315")
-
     ## plottply help
     mod_help_plot_server("help")
 
-    ## ICUs VE
-
-    if (super_secret()[["permission"]][usr_pos] %in% c("ubep", "tip-v")) {
-      mod_icuve_sitrep_server("icuve_sitrep")
-      mod_azero_server("icuve_sitrep")
-      mod_icuve_ts_server("icuve_ts")
-      mod_icuve_static_server("icuve_static")
-    }
-
-    if (super_secret()[["permission"]][usr_pos] %in% c("ubep", "tip-v", "agenas")) {
-      mod_tsicuve_server("partial_ts_icuve")
-      mod_reg_tsicuve_server("regional_partial_tsicuve")
-      mod_reg_nocritica_server("regional_partial_nocritica")
-    }
-
-    ## National
-    mod_ts_ita_server("ts_nat_cum", "cum")
-    mod_ts_ita_server("ts_nat_inc", "inc")
-
-    # ## Regional
-    mod_ts_reg_server("ts_reg_cum_mes", "cum", color_var = "measure")
-    mod_ts_reg_server("ts_reg_inc_mes", "inc", color_var = "measure")
-    mod_ts_reg_server("ts_reg_cum_reg", "cum", color_var = "region")
-    mod_ts_reg_server("ts_reg_inc_reg", "inc", color_var = "region")
-
-    ## Provincial
-    mod_ts_prv_server("ts_prv_cum", "cum")
-    mod_ts_prv_server("ts_prv_inc", "inc")
 
 
-    ## In evidenza
-    mod_0314_server("dapb")
-    mod_0318_friuli_server("20200318_fvg")
-    mod_0318_piemonte_server("20200318_piemonte")
-    mod_0318_intensive_server("21")
-    mod_0320_novara_server("da_novara")
-    mod_0320_novara_server("da_vercelli",
-                           loc = "Vercelli",
-                           pop = 174904
-    )
-    mod_0320_novara_server("da_alessandria",
-                           loc = "Alessandria",
-                           pop = 428826
-    )
-    mod_0323_picco_server("picco")
-    mod_0325_hosp_server("hosp")
-    mod_0328_hosp_server("tot")
-    mod_0331_server("ven_pie")
-    mod_0404_magnani_server("mortality")
-    mod_0406_mort_veneto_server("mort_veneto")
-    mod_0415_tamponi_server("tamp_hosp")
 
-    ## Geo-spatial
-    mod_maps_server("geo_1")
+
+
+
+
+
+
+
+
+
+
+
 
   })
 
@@ -278,15 +288,6 @@ app_server <- function(input, output, session) {
 
         menuItem("Archivio analisi",
                  icon = icon("bullseye"),
-
-                 if (super_secret()[["permission"]][usr_pos] %in% c("ubep", "tip-v", "agenas")) {
-
-                   menuSubItem("Veneto partial timeseries",
-                               tabName = "partial-ts-icuve",
-                               icon = icon("map")
-                   )
-                 },
-
 
                  menuSubItem(
                    text = "2020-04-15 Impatto tamponi",
@@ -489,7 +490,7 @@ app_server <- function(input, output, session) {
             tabName = "azero-sitrep",
             h2("Report occupazione e permanenza in area critica."),
             if (super_secret()[["permission"]][usr_pos] %in% c("ubep", "tip-v")) {
-              mod_azero_ui("icuve_sitrep")
+              mod_azero_ui("azero_sitrep")
             }
           ),
           tabItem(
